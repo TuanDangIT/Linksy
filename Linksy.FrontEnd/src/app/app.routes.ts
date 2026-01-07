2b6a763e9214fa141237b529d2d7ef71fd35f8c2
TuanDangIT <anh_tuan_dang@outlook.com>
2026-01-07 22:48:37 +0100

    Change default route for front.

--- Linksy.FrontEnd/src/app/app.routes.ts
+++ Linksy.FrontEnd/src/app/app.routes.ts
@@ -62,5 +62,5 @@ export const routes: Routes = [
       { path: 'barcodes', component: BarcodeList, title: 'Barcodes', canActivate: [authGuard] },
     ],
   },
-  { path: '**', redirectTo: '/login' },
+  { path: '**', redirectTo: '/shortened-urls' },
 ];
